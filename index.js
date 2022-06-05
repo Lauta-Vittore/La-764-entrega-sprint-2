c85431f61ea97378e04a4e52e1b6ce6d6fe9244d
Lauta-Vittore <lautarotin@gmail.com>
2022-06-04 23:24:16 -0300

    actualizacion

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
 window.onload = function() {
-    window.nombre = [];
-    window.cant = [] ;
+    window.nombre = []; //creo array para guardar nombre     
+    window.cant = [] ; //creo array para guardar cantidad gastada
 }
 
 
 function agregarNombre() {
-    var total = 0;
-    let pers = 0;
-    let i = 0;
-    let opcion;
-    let dato = document.getElementById('texto').value;
-    let dat = parseInt(document.getElementById('numero').value);
+    var total = 0; //variable para guardar cantidad todal gastada
+    let pers = 0; // variable para guardar cantidad de personas
+    let text; // variable paraguardar cadena de texto 
+    let dato = document.getElementById('texto').value; //tomo el nombre ingresado y lo guardo en la variable dato
+    let dat = parseInt(document.getElementById('numero').value); //tomo el numero ingresado y lo guardo en la variable dat
     
-    if((dato != "") && (dat >= 0)){
-        nombre.push(dato);
-        cant.push(dat);
-    for(let i = 0; i< cant.length; i++){
+    if((dato != "") && (dat >= 0)){ //condicion: si dato es distinto de nada Y dat mayor o igual a cero
+        nombre.push(dato); //guardo dato en el array nombre 
+        cant.push(dat);// guardo dat en el array cant
+    for(let i = 0; i< cant.length; i++){ 
         
-        total += cant[i];
-        pers++;
-        opcion = "<li>"  +nombre[i]+ ":" +" $" + cant[i] + "</li>";
+        total += cant[i]; //suma de la cantidad total
+        pers++; //conteo de cantidad de personas
+        text = "<li>"  +nombre[i]+ ":" +" $" + cant[i] + "</li>"; // guardado de texto en text
     }
 
-    let lista = document.getElementById('lista-nombres');
-    lista.innerHTML += opcion;
+    let lista = document.getElementById('lista-nombres'); // indico donde insertar el texto
+    lista.innerHTML += text; //inserto el texto en el html
     
-    document.getElementById('el-resultado').innerHTML = "$"+total;
-    document.getElementById('aporta').innerHTML ="$"+ ((total/pers).toFixed(2));
-    document.getElementById("texto").value = "";
-    document.getElementById("numero").value = "";
+    document.getElementById('el-resultado').innerHTML = "$"+total; //inserto el total en el html
+    document.getElementById('aporta').innerHTML ="$"+ ((total/pers).toFixed(2)); //inserto y calculo el promedio y le indico que van solo 2 numeros despues de la coma
+    document.getElementById("texto").value = ""; //limpiar celda de nombre      
+    document.getElementById("numero").value = ""; //limpiar celda de cantidad
     repartir(total);
 }else{
-        alert("ingresar nombre y numero positivo");
-    }
-}
-
-function repartir(total){
-    let sobra;
-    for(let i = 0 ; i < cant.length ; i++){
-        
-        if(cant[i]>total){
-            alert(cant[i]);
-            sobra = cant[i] - total; 
-            document.getElementById('aporte').innerHTML = nombre[i] + "le sobran" + "$" + sobra;
-            alert(nombre[i] + "le sobran" + "$" + sobra);
-        }
+        alert("ingresar nombre y numero positivo"); //si no se cumple la condicion alerta
     }
 }
